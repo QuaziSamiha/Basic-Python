64e683e56e45fe22dbaee0151662aa98d52d35b8
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-06-02 12:17:30 +0600

    variable and its type

---
+++ Variable.py
@@ -0,0 +1,28 @@
+a = 12
+b = "Samiha"
+c = 23.53
+
+print(a+3)
+
+print(a+c)
+
+'''
+Rules for Declaring Variables:
+
+1. Variable should start with a letter or an underscore
+2. Variable cannot contain alpha numeric characters
+3. It can only contain alpha numeric characters
+4. Variables names are case sensitive
+
+'''
+
+# Idntifying type of a variable
+
+typeA = type(a)
+print("Type a : ", typeA)
+
+print("Type b: ", type(b))
+
+a = "31"  # changing variable type is possible
+print(a)
+print("Type a : ", type(a))
