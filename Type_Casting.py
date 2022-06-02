6d8906e8de66bf085c2ceba043845961ee9a14ac
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-06-02 16:25:12 +0600

    type casting done

---
+++ Type_Casting.py
@@ -0,0 +1,19 @@
+v1 = "30"
+
+# string to integer conversion
+v1 = int(v1)
+
+print("Type v1: ", type(v1))
+
+print(v1+10)
+print(int("503"))
+
+# integer to float conversion
+v1 = float(v1)
+
+print("Type v1: ", type(v1))
+
+print(v1+10)
+
+# integer to string
+print(str(333))
