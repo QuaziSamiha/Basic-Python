1b30ee16526d93076374654f4ee711621e6795c1
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-06-03 13:04:45 +0600

    Tuple in python starts

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+tpl = ("Sammu", "Harry", "Marry")
+print(tpl)
+print(type(tpl))
+
+# tuple is not changeable
+
+# tpl[0] = "Vondul" # not allowed
+
+# type casting
+lst = list(tpl)
+print(type(lst))
